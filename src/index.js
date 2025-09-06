ed1736c295702f8ed14358ff73b251b3b6a92bae
lucaradio24 <lukaabeck3@gmail.com>
2025-09-06 20:17:30 +0200

    Created logic for tab switching

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import "./styles.css";
+import { loadPage } from './home.js'
 
-console.log('Hello')
+import { loadMenu } from './menu.js'
 
-// Andare avanti coi passi
-// Poi quando funziona la logica del cambio tab
-// Allora sistemo il css per bene
+import { loadAbout } from "./about.js";
+
+
+const navBtn = document.querySelectorAll('.nav-btn');
+// for (let i = 0; i < navBtn.length; i++){
+//     navBtn[i].addEventListener('click', () => {
+//         content.innerHTML = '';
+//         if (navBtn[i].dataset.tab === 'home'){
+//             loadPage()
+//         } else if(navBtn[i].dataset.tab === 'menu'){
+//             loadMenu()
+//         } else {
+//             loadAbout()
+//         }
+            
+        
+//     })
+// }
+
+
+navBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        content.innerHTML = '';
+        if (btn.dataset.tab === 'home'){
+            loadPage()
+        } else if (btn.dataset.tab === 'menu'){
+            loadMenu()
+        } else {
+            loadAbout()
+        }
+    })
+})
+
+loadPage()
